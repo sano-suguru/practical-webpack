4574546e9add49c559668c3c09357590382e3c53
nossa <sa.50.00.no.riku+github@gmail.com>
2020-09-02 01:38:38 +0900

    4-26

---
+++ src/js/another.js
@@ -0,0 +1,7 @@
+import $ from "jquery";
+import velocity from "velocity-animate";
+
+$("body").css("color", "red");
+velocity($("h1"), "fadeIn", { duration: 2000, loop: true });
+
+// console.log(a);
